bd266b16e6078226c59a5f267f56faaa8331ef88
Carlos Bonilla <carlosbonilla@isboli.com>
2019-04-09 00:12:27 -0600

    List items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Items from "./components/Items";
+
+import "./App.css";
+
+const list = {
+  item1: {
+    name: "Pan"
+  },
+  item2: {
+    name: "Huevos"
+  },
+  item3: {
+    name: "Mantequilla"
+  },
+  item4: {
+    name: "Azucar"
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Simple{" "}
+            <span role="img" aria-label="coffee">
+              ☕
+            </span>{" "}
+            List
+          </h1>
         </header>
+        <section className="buy">
+          <ul className="dont-forget">
+            {Object.keys(list).map(key => (
+              <Items key={key} index={key} item={list[key]} />
+            ))}
+          </ul>
+        </section>
+        <section className="bought">
+          <ul className="i-remember" />
+        </section>
       </div>
     );
   }
